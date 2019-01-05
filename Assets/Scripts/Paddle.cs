b90a11a8af8c1d55c129b3b3d09e9c19d6e9e31a
KarowM <karow.foxx@gmail.com>
2019-01-05 18:29:46 +0000

    First commit

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log(Screen.width);
+        Debug.Log(Screen.height);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Debug.Log(Input.mousePosition.x);
+    }
+}
