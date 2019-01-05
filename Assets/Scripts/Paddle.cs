b054475596fe62c69d784f032b8d933879e37172
KarowM <karow.foxx@gmail.com>
2019-01-05 19:08:19 +0000

    paddle follows mouse

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
     // Start is called before the first frame update
+    [SerializeField]float  screenWidthInUnits = 16f;
+
     void Start() {
         Debug.Log(Screen.width);
         Debug.Log(Screen.height);
@@ -11,6 +13,7 @@ public class Paddle : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        //Debug.Log(Input.mousePosition.x);
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        transform.position = new Vector2(mousePosInUnits, transform.position.y); ;
     }
 }
