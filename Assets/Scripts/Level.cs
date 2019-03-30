180450be51a971bce6f88d312e08a000d126985f
KarowM <karow.foxx@gmail.com>
2019-03-30 20:58:32 +0000

    when blocks run out, next scene is loaded

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,11 +4,20 @@ public class Level : MonoBehaviour {
 
     [SerializeField] int numBlocks;
 
+    SceneLoader sceneLoader;
+
+    private void Start() {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
     public void addBlock() {
         numBlocks++;
     }
 
     public void removeBlock() {
         numBlocks--;
+        if (numBlocks == 0) {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
