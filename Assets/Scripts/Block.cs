1a9ef4d7f3b6da5273c6045cb2eb18934793fe98
KarowM <karow.foxx@gmail.com>
2019-03-30 20:31:29 +0000

    added prefab for level to track num of blocks left, removed unused imports

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Block : MonoBehaviour {
 
     [SerializeField] AudioClip breakSound;
 
+    Level level;
+
+    private void Start() {
+        level = FindObjectOfType<Level>();
+        level.addBlock();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision) {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position, 1);
+        level.removeBlock();
         Destroy(gameObject);
     }
 
---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    [SerializeField] int numBlocks;
+
+    public void addBlock() {
+        numBlocks++;
+    }
+
+    public void removeBlock() {
+        numBlocks--;
+    }
+}
